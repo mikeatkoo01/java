41de822a67a9bf21f0c236e025ff8ec8fa9160a5
unknown <mikeatkoo01@gmail.com>
2024-01-12 12:03:29 +0000

    helloMA

--- src/task1/taskone.java
+++ src/task1/taskone.java
@@ -3,6 +3,41 @@ package task1;
 public class taskone {
 
 	public static void main(String[] args) {
+		byte bite = 25;
+		System.out.println(bite);
+		short shortie = 255;
+		System.out.println(shortie);
+		int intee = 1_2_000_000;
+		System.out.println(intee);
+		long longer = 1234567891;
+		System.out.println(longer);
+		
+		float floaty = 44.94F;
+		System.out.println(floaty);
+		double doublebubble = 44.94;
+		System.out.println(doublebubble);
+		
+		boolean b = false;
+		System.out.println(b);
+		boolean t = true;
+		System.out.println(t);
+		
+		
+		char m = 'm';
+		char r = 'r';
+		System.out.println(m + r);
+		String s = "mike";
+		System.out.println(s);
+		
+		String a = "alfie";
+		String k = "kian;";
+				System.out.println(a + k);
+				
+				
+				
+	
+
+		
 		
 	}
 }
